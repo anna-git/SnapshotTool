91e3a98a8898cafc33a9ed50fc28370708a1fb02
Anna <anna.yafi@datadoghq.com>
2022-06-16 14:56:02 +0200

    init

---
+++ SnapshotTool/Program.cs
@@ -0,0 +1,23 @@
+﻿var folder = args[0];
+var d = new DirectoryInfo(folder);
+foreach (var file in d.GetFiles())
+{
+// run under linux if file path is too long, pb with win32 apis
+    if (file.Name.EndsWith("verified.txt"))
+    {
+        file.Delete();
+    }
+}
+
+var destfolder = args[1];
+
+foreach (var file in d.GetFiles())
+{
+    if (file.Name.EndsWith("received.txt"))
+    {
+        var n = file.Name;
+        var newName = n.Replace("received.txt", "verified.txt");
+        var newPath = Path.Combine(destfolder, newName);
+        file.MoveTo(newPath, true);
+    }
+}
